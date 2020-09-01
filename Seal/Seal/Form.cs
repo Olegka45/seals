0213701e2d8b8e637ce6c82efb9b30b59eef4ce7
Olegka45 <Olegka45@users.noreply.github.com>
2020-09-01 10:37:47 +0300

    Added report panel + some design changes

--- Seal/Seal/Form.cs
+++ Seal/Seal/Form.cs
@@ -16,5 +16,48 @@ namespace Seal
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void CheckBoxPlace_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBoxPlace.Checked)
+            {
+                textBoxPlace.Visible = true;
+            }
+            else
+            {
+                textBoxPlace.Visible = false;
+            }
+        }
+
+        private void CheckBoxTime_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxTime.Checked)
+            {
+                textBoxTime.Visible = true;
+            }
+            else
+            {
+                textBoxTime.Visible = false;
+            }
+        }
+
+        private void CheckBoxNumber_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBoxNumber.Checked)
+            {
+                textBoxNumber.Visible = true;
+            }
+            else
+            {
+                textBoxNumber.Visible = false;
+            }
+        }
     }
 }
