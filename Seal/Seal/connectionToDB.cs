a42fc5a27b76ffa84782d229e5adf555b12d6512
Olegka45 <Olegka45@users.noreply.github.com>
2020-09-01 09:54:57 +0300

    connection writen

--- Seal/Seal/connectionToDB.cs
+++ Seal/Seal/connectionToDB.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace Seal
 {
-    class connectionToDB
+    class connectionTODB
     {
+        //string connectString = "server=localhost;user=root;database=PLOMBS;port=3306;password=1945";
+        MySqlConnection myConnection = new MySqlConnection("server=localhost;user=root;database=PLOMBS;port=3306;password=1945");
+        public void connectionCreate()
+        {
+            myConnection.Open();
+        }
+
+        public void Execute(string query)
+        {
+            try
+            {
+
+                MySqlCommand command = new MySqlCommand(query, myConnection);
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (query == "")
+                {
+                    MessageBox.Show("");
+                }
+                else
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        public void connectionClose()
+        {
+            myConnection.Close();
+        }
     }
 }
